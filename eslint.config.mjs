0cb33811577130f32cf03647b804c3ca64280eff
nsx07 <felipeaugustodluz@hotmail.com>
2025-03-06 22:26:56 -0300

    adding base

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,27 +9,6 @@ export default tseslint.config(
     ignores: ['eslint.config.mjs'],
   },
   eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
-  {
-    languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.jest,
-      },
-      ecmaVersion: 5,
-      sourceType: 'module',
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-  },
-  {
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
-    },
-  },
+  // ...tseslint.configs.recommendedTypeChecked,
+  // eslintPluginPrettierRecommended,
 );
